ad11e819a18a5c5cda6cf176fb913252e8124109
ehgalter <ehgalter@gmail.com>
2020-05-09 00:56:42 +0100

    Initial Commit Tailwind + NextJS

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        gray: '#c7d0d8',
+        jungle: '#1a1d28',
+        gunmetal: '#2e3142',
+        green: '#43bc70',
+        red: '#f25f5c'
+      },
+      textColor: {
+        primary: '#fff',
+        secondary: '#c7d0d8',
+      },
+      container: {
+        center: true,
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
